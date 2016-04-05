06c326a4f1954875f804e84f590a3bf273076b2f
G.W. van der Vegt <wvd_vegt@180950.no-reply.drupal.org>
2016-04-05 14:31:02 +0200

    1) Introduced new IWebServiceRequest2 interface to replace previous one. 2) The new interface is easier to use as it looks sync to the asset (also in Bridge it can be used sync or async). 3) The new interface returns a full description of the response. 4) Still untested on mobile devices / xamarin and unity.

---
+++ RageAssetManager/IWebServiceRequest2.cs
@@ -0,0 +1,136 @@
+﻿namespace AssetPackage
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Request Settings.
+    /// </summary>
+    public class RequestSetttings
+    {
+        /// <summary>
+        /// The method.
+        /// </summary>
+        public string method;
+
+        /// <summary>
+        /// URI of the document.
+        /// </summary>
+        public Uri uri;
+
+        /// <summary>
+        /// The request headers.
+        /// </summary>
+        public Dictionary<String, String> requestHeaders;
+
+        /// <summary>
+        /// The body.
+        /// </summary>
+        public String body;
+
+        /// <summary>
+        /// The allowed responses.
+        /// </summary>
+        public List<int> allowedResponsCodes;
+
+        /// <summary>
+        /// Initializes a new instance of the AssetPackage.requestParameters
+        /// class.
+        /// </summary>
+        public RequestSetttings()
+        {
+            method = "GET";
+            requestHeaders = new Dictionary<String, String>();
+            body = String.Empty;
+            allowedResponsCodes = new List<int>();
+            allowedResponsCodes.Add(200);
+        }
+    }
+
+    /// <summary>
+    /// Response results.
+    /// </summary>
+    public class RequestResponse : RequestSetttings
+    {
+        /// <summary>
+        /// The response code.
+        /// </summary>
+        public int responseCode;
+
+        /// <summary>
+        /// Message describing the respons.
+        /// </summary>
+        public string responsMessage;
+
+        /// <summary>
+        /// The response headers.
+        /// </summary>
+        public Dictionary<String, String> responseHeaders;
+
+        /// <summary>
+        /// Initializes a new instance of the AssetPackage.RequestResponse class.
+        /// </summary>
+        public RequestResponse() : base()
+        {
+            responseCode = 0;
+            responsMessage = String.Empty;
+
+            responseHeaders = new Dictionary<String, String>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AssetPackage.RequestResponse class.
+        /// </summary>
+        ///
+        /// <remarks>
+        /// The body is not copied as it will contain thee response body instead.
+        /// </remarks>
+        ///
+        /// <param name="settings"> Options for controlling the operation. </param>
+        public RequestResponse(RequestSetttings settings) : this()
+        {
+            method = settings.method;
+            requestHeaders = settings.requestHeaders;
+            uri = settings.uri;
+            body = String.Empty;
+
+            allowedResponsCodes = settings.allowedResponsCodes;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether result is allowed.
+        /// </summary>
+        ///
+        /// <value>
+        /// true if result allowed, false if not.
+        /// </value>
+        public bool ResultAllowed
+        {
+            get
+            {
+                return allowedResponsCodes.Contains(responseCode);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interface for web service request.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// Implemented on a Bridge.
+    /// </remarks>
+    public interface IWebServiceRequest2
+    {
+        /// <summary>
+        /// Web service request.
+        /// </summary>
+        ///
+        /// <param name="requestSettings">  Options for controlling the operation. </param>
+        ///
+        /// <returns>
+        /// A RequestResponse.
+        /// </returns>
+        void WebServiceRequest(RequestSetttings requestSettings, out RequestResponse requestResponse);
+    }
+}
