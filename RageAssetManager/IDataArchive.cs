56d44423c0ec89ea496c50b0bca7954b71b044a6
wvdv <wim@vander-vegt.nl>
2018-10-02 14:35:01 +0200

    1) Added missing IDataArchive interface. 2) Improved pdb2mdb PostBuildEvent task in project file.

---
+++ RageAssetManager/IDataArchive.cs
@@ -0,0 +1,23 @@
+// <copyright file="IDataArchive.cs" company="RAGE"> Copyright (c) 2015 RAGE. All rights reserved.
+// </copyright>
+// <author>Veg</author>
+// <date>13-4-2015</date>
+// <summary>Defines the IDataStorage Interface</summary>
+namespace AssetPackage
+{
+    using System;
+
+    public interface IDataArchive
+    {
+        /// <summary>
+        /// Archives the given file.
+        /// </summary>
+        ///
+        /// <param name="fileId"> The file identifier to delete. </param>
+        ///
+        /// <returns>
+        /// true if it succeeds, false if it fails.
+        /// </returns>
+        Boolean Archive(String fileId);
+    }
+}
